e084d9cb840364069303326cca1f3f1c71aaa37b
apoorva-27 <apoorva27@ccs.neu.edu>
2018-05-20 05:08:09 -0400

    initial commit

---
+++ src/main/java/edu/northeastern/cs5200/cs5200summer2018nagaraj/Cs5200Summer2018NagarajApplication.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5200.cs5200summer2018nagaraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200Summer2018NagarajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Summer2018NagarajApplication.class, args);
+	}
+}
