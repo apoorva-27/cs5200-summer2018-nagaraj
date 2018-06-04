5aeca9bfdc8b7a2995dc296b242334bf88c74be6
apoorva-27 <apoorva27@ccs.neu.edu>
2018-06-03 20:38:24 -0400

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/cs5200summer2018nagaraj/Cs5200Summer2018NagarajApplication.java
+++ src/main/java/edu/northeastern/cs5200/cs5200summer2018nagaraj/Cs5200Summer2018NagarajApplication.java
@@ -2,11 +2,20 @@ package edu.northeastern.cs5200.cs5200summer2018nagaraj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Summer2018NagarajApplication {
+public class Cs5200Summer2018NagarajApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Summer2018NagarajApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources ( Cs5200Summer2018NagarajApplication.class);
+	}
+
 }
--- src/main/java/edu/northeastern/cs5200/cs5200summer2018nagaraj/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/cs5200summer2018nagaraj/controllers/hello/HelloController.java
@@ -6,10 +6,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @RestController
-
 public class HelloController {
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @RequestMapping("/api/hello/insert")
+    public HelloObject insertHelloObject() {
+        HelloObject obj = new HelloObject("Hello Apoorva Nagaraj!");
+        helloRepository.save(obj);
+        return obj;
+    }
+
+    @RequestMapping("/api/hello/select/all")
+    public List<HelloObject> selectAllHelloObjects(){
+        List<HelloObject> hellos = (List<HelloObject>) helloRepository.findAll();
+        return hellos;
+    }
+
+    @RequestMapping("api/hello/insert/{msg}")
+    public HelloObject insertMessage(@PathVariable("msg") String message)
+    {
+        HelloObject obj=new HelloObject(message);
+        helloRepository.save(obj);
+        return obj;
+    }
 
     @RequestMapping("/api/hello/string")
 
